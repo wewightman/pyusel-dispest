47b09550958b96f9cfa62b3384c51dcb4be85e4c
Wren Wightman <wren.wightman@gmail.com>
2023-05-13 23:43:33 -0400

    Fixed packaging for PyPI

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
 from setuptools import setup
 
-# load requirements list
-with open("requirements.txt", 'r') as f:
-    reqs = f.readlines()
-
-with open("req_depends.txt", 'r') as f:
-    reqdeps = f.readlines()
-
 with open("README.md", 'r') as f:
     readme = f.read()
 
 # run setup tools
 setup(
-    name="dispest",
-    description="Displacement estimators and shear wave speed estimators",
+    name="pyusel-dispest",
+    description="Displacement estimators used in ultrasound elasticity",
     author="Wren Wightman",
     author_email="wew12@duke.edu",
-    install_requires=reqs,
-    dependency_links=reqdeps,
     readme=readme,
-    version="0.0.0"
+    version="0.0.1"
 )
