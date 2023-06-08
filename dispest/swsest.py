85b4088f66b6691e238edc767ab21d24d62fe72d
Wren Wightman <wren.wightman@duke.edu>
2023-06-08 14:46:35 -0400

    Working radon

--- dispest/swsest.py
+++ dispest/swsest.py
@@ -23,29 +23,41 @@ def swsradon(spctm, lat, t, latmin, latmax, tmin, tmax, N:int=512, speedonly:boo
     `(speed, radonsum, t)`: estimated speed in m/s, energy integration chart, and time vector along the edges
     """
     from scipy.interpolate import interp1d as i1d
+    from scipy.interpolate import RectBivariateSpline as RBS
     import numpy as np
 
+    t = np.array(t).flatten()
     trange = np.linspace(tmin, tmax, N)
-    lat = np.array(lat)
-    latmask = (lat >= latmin) & (lat <= latmin)
-    # f_set = [i1d()]
 
-    # f_set = RGI((lat, t), spctm, method='cubic', bounds_error=False, fill_value=0)
-    # sums = np.zeros((N,N))
+    # Find the number of lateral positions contained within given range
+    lat = np.array(lat).flatten()
+    latmask = (lat >= latmin) & (lat <= latmax)
+    nlat = np.sum(latmask)
+    if nlat == 0:
+        raise ValueError(f"Latmask range of {latmin} to {latmax} does not contain any points")
+    latmin = np.min(lat[latmask])
+    latmax = np.max(lat[latmask])
+    lateval = np.linspace(latmin, latmax, nlat)
 
-    # for it1 in range(N):
-    #     for it2 in range(it1+1, N):
-    #         teval = np.linspace(trange[it1], trange[it2], N)
-    #         eval = np.array([teval, lateval]).T
-    #         sums[it1, it2] = np.sum(f(eval))
+    f = RBS(lat, t, spctm)
+
+    sums = np.zeros((N,N))
+
+    for it1 in range(N):
+        for it2 in range(it1+1, N):
+            teval = np.linspace(trange[it1], trange[it2], nlat)
+            sums[it1, it2] = np.sum(f(lateval, teval, grid=False))
     
-    # imax = np.where(sums == np.max(sums))
-    # st1, st2 = imax[0]
+    imax = np.where(sums == np.max(sums))
+    if len(imax[0]) != 2:
+        st1, st2 = imax
+    else:
+        st1, st2 = imax[0]
 
-    # c = (latmax-latmin)/(teval[st2]-teval[st1])
+    c = (latmax-latmin)/(trange[st2]-trange[st1])
 
-    # if speedonly:
-    #     return c
-    # else:
-    #     return c, sums, teval
+    if speedonly:
+        return c
+    else:
+        return c, sums, trange
     
